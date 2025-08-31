fc70e125d3959edba47b0cb5e5ab2a28a345d52b
T2Sniper <ahmed.atya.daif@gmail.com>
2025-08-31 21:48:03 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const services = [
     id: 1,
     name: 'استشارة',
     description: 'استشارة طبية لتقييم حالة الأسنان.',
-    image: 'https://via.placeholder.com/300x180?text=استشارة',
+    image: 'https://i.ibb.co/Ng6Qw72N/image.png',
     duration: '30 دقيقة',
     benefits: 'تشخيص دقيق، خطة علاج مخصصة.',
     moreInfo: 'استشارة مع أطباء متخصصين.',
@@ -857,6 +857,7 @@ if (mapSection) {
 
 // دعم اللغة الافتراضية
 document.documentElement.lang = navigator.language || 'ar';
+
 
 
 
